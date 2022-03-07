3a72d13047c355316a028221e133b636fb1f5d51
Tim Roeder <timothyjroeder@gmail.com>
2022-03-06 23:30:01 -0700

    Request: backgrounds#index action return image

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,3 +1,12 @@
-class BackgroundsController < ApplicationController
-  
+class Api::V1::BackgroundsController < ApplicationController
+  def index
+    @background = BackgroundFacade.get_location_image(loc_params[:location])
+    json_response(BackgroundSerializer.new(@background))
+  end
+
+  private
+
+  def loc_params
+    params.permit(:location)
+  end
 end
--- spec/requests/api/v1/backgrounds_request_spec.rb
+++ spec/requests/api/v1/backgrounds_request_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
-RSpec.describe 'Backgrounds Request API', type: :request do
+RSpec.describe 'Backgrounds Request API', :vcr, type: :request do
+  context '#index action' do
+    it 'returns a successful status when given valid location' do
+      get api_v1_backgrounds_path(params: { location: 'denver,co' })
+      
+      expect(response).to be_successful
+    end
+    
+    it 'returns parsable json data response for background' do
+      get api_v1_backgrounds_path(params: { location: 'denver,co' })
+      
+      expect(json_parse(response.body)).to be_a(Hash)
+    end
+    
+    it 'returns correct response attribute fields' do
+      get api_v1_backgrounds_path(params: { location: 'denver,co' })
+      hash = json_parse(response.body)
+      attr = hash[:data][:attributes]
   
+      expect(hash).to have_key(:data)
+      expect(hash[:data]).to have_key(:id)
+      expect(hash[:data][:id]).to eq(nil)
+      expect(hash[:data][:type]).to eq('image')
+  
+      expect(attr).to have_key(:image)
+  
+      expect(attr[:image]).to have_key(:image_id)
+      expect(attr[:image]).to have_key(:owner_id)
+      expect(attr[:image]).to have_key(:secret_id)
+      expect(attr[:image]).to have_key(:title)
+      expect(attr[:image]).to have_key(:location)
+      expect(attr[:image]).to have_key(:image_links)
+      expect(attr[:image]).to have_key(:credit)
+      
+      image_links = attr[:image][:image_links]
+      
+      expect(image_links).to have_key(:large_url)
+      expect(image_links).to have_key(:large_height)
+      expect(image_links).to have_key(:large_width)
+      expect(image_links).to have_key(:thumbnail_url)
+      expect(image_links).to have_key(:thumbnail_height)
+      expect(image_links).to have_key(:thumbnail_width)
+
+      credit = attr[:image][:credit]
+
+      expect(credit).to have_key(:owner_name)
+      expect(credit).to have_key(:license)
+      expect(credit).to have_key(:license_url)
+      expect(credit).to have_key(:source)
+      expect(credit).to have_key(:notice)
+      expect(credit).to have_key(:terms_url)
+    end
+  end
 end
+
