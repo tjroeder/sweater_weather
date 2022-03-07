f12ed90937e621f18649a02121ec5a502af3237d
Tim Roeder <timothyjroeder@gmail.com>
2022-03-06 23:22:42 -0700

    Test: create json parse helper

--- spec/support/request_spec_helper.rb
+++ spec/support/request_spec_helper.rb
@@ -1,5 +1,5 @@
 module RequestSpecHelper
-  def json_parse
-    JSON.parse(response.body, symbolize_names: true)
+  def json_parse(response)
+    JSON.parse(response, symbolize_names: true)
   end
 end
