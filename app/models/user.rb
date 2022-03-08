b2ebe157d3413c6255c9aeb40c2436a20ebe3082
Tim Roeder <timothyjroeder@gmail.com>
2022-03-07 19:12:14 -0700

    Model: removed unnessesary validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :api_key
   validates :email, email: true, uniqueness: true, presence: true
-  validates :password, confirmation: true, presence: true, length: { minimum: 8 }
-  validates :password_confirmation, presence: true, length: { minimum: 8 }
+  validates :password_digest, confirmation: true, presence: true
+  validates :password, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,12 +6,9 @@ RSpec.describe User, type: :model do
     it { should have_secure_token :api_key }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
-    it { should validate_presence_of :password }
+    it { should validate_confirmation_of :password_digest }
     it { should validate_length_of(:password).is_at_least(8) }
-    it { should validate_confirmation_of :password }
-    it { should validate_confirmation_of :password }
     it { should validate_presence_of :password_confirmation }
-    it { should validate_length_of(:password_confirmation).is_at_least(8)  }
   end
 
   describe 'factorybot' do
