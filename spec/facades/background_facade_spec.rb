119524e87fcca6e4aa45bb8026aabceb0083b58c
Tim Roeder <timothyjroeder@gmail.com>
2022-03-08 23:29:51 -0700

    Facade: update for no image edge case

--- spec/facades/background_facade_spec.rb
+++ spec/facades/background_facade_spec.rb
@@ -8,6 +8,15 @@ RSpec.describe BackgroundFacade, type: :facade do
       it 'returns a single Background object' do
         expect(background).to be_a(Background)
       end
+
+      it 'returns obj with title no image found if no images found' do
+        broken =  BackgroundFacade.get_location_image('asdfasdfsdfasdfasdfs')
+
+        expect(broken).to be_a(Background)
+        expect(broken.title).to eq('NO IMAGE FOUND')
+        expect(broken.image[:title]).to eq('NO IMAGE FOUND')
+        expect(broken.image[:location]).to eq('asdfasdfsdfasdfasdfs')
+      end
     end
   end
 end
