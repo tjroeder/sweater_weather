dcd2fabcfa16973ddf1014c21c759de0b6ab1968
Tim Roeder <timothyjroeder@gmail.com>
2022-03-09 01:36:41 -0700

    Facade: update for location errors

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,12 +1,17 @@
 class ForecastFacade
   def self.get_location_forecast(city_state)
     location = ForecastService.get_city_lat_lon(city_state)
-    lat = location[:results][0][:locations][0][:latLng][:lat]
-    lng = location[:results][0][:locations][0][:latLng][:lng]
+    
+    if location[:results][0][:locations][0][:adminArea5].present?
+      lat = location[:results][0][:locations][0][:latLng][:lat]
+      lng = location[:results][0][:locations][0][:latLng][:lng]
 
-    forecast = Forecast.new(ForecastService.get_forecast(lat, lng, 'imperial'))
-    forecast.daily_weather.slice!(-3..-1)
-    forecast.hourly_weather.slice!(-40..-1)
-    forecast
+      cast = Forecast.new(ForecastService.get_forecast(lat, lng, 'imperial'))
+      cast.daily_weather.slice!(-3..-1)
+      cast.hourly_weather.slice!(-40..-1)
+      cast
+    else
+      Forecast.new({})
+    end
   end
 end
--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -1,24 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe ForecastFacade, type: :facade do
-  let!(:forecast) { ForecastFacade.get_location_forecast('denver,co') }
-
+  
   context 'class methods', :vcr do
     describe '::get_location_forecast' do
-      it 'returns a Forecast object' do
-        expect(forecast).to be_a(Forecast)
+      context 'valid location request' do
+        let!(:forecast) { ForecastFacade.get_location_forecast('denver,co') }
+
+        it 'returns a Forecast object' do
+          expect(forecast).to be_a(Forecast)
+        end
+  
+        it 'returns a Forecast object with the the current weather' do
+          expect(forecast.current_weather.count).to eq(10)
+        end
+  
+        it 'returns a Forecast object with the next 5 days of weather data' do
+          expect(forecast.daily_weather.count).to eq(5)
+        end
+  
+        it 'returns a Forecast object with the next 8 hours of weather data' do
+          expect(forecast.hourly_weather.count).to eq(8)
+        end
       end
 
-      it 'returns a Forecast object with the the current weather' do
-        expect(forecast.current_weather.count).to eq(10)
-      end
+      context 'invalid location request' do
+        it 'returns blank Forecast with error messages' do
+          bad_location = ForecastFacade.get_location_forecast('zzzzzzzzzzzzzz')
 
-      it 'returns a Forecast object with the next 5 days of weather data' do
-        expect(forecast.daily_weather.count).to eq(5)
-      end
+          expected_error = { errors: [ { status: 400, title: 'Bad Request' }] }
 
-      it 'returns a Forecast object with the next 8 hours of weather data' do
-        expect(forecast.hourly_weather.count).to eq(8)
+          expect(bad_location).to be_a(Forecast)
+          expect(bad_location.current_weather).to eq(expected_error)
+          expect(bad_location.daily_weather).to eq(expected_error)
+          expect(bad_location.hourly_weather).to eq(expected_error)
+        end
       end
     end
   end
